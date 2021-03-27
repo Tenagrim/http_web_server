3a5373e6a1491788126fd56a38aa6ce2160489a9
Tenagrim <tenagrim@gmail.com>
2021-03-27 07:01:51 +0300

    comments in IFileManager (forgot to save)

--- webserv/interfaces/IFileManager.hpp
+++ webserv/interfaces/IFileManager.hpp
@@ -1,9 +1,19 @@
 #pragma once
 #include <string>
+#include <time.h>
 class IFileManager
 {
 	public:
 		virtual	bool			isFileExisting(std::string const &filename) = 0;
 		virtual unsigned int	getFileSize(std::string const &filename) = 0;
+
+		// as in http header content-type
 		virtual std::string		getContentType(std::string const &filename) = 0;
+
+		// Last modification time 
+		virtual time_t			getMTime(std::string const &filename) = 0;
+
+		// Last change time 
+		virtual time_t			getCTime(std::string const &filename) = 0;
+		
 };
