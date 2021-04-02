69d5b1b93a29eb2ef80c90f58f6711829f102fba
Tenagrim <tenagrim@gmail.com>
2021-04-02 10:03:29 +0300

    Request validator interface changed

--- webserv/interfaces/IRequestValidator.hpp
+++ webserv/interfaces/IRequestValidator.hpp
@@ -12,7 +12,10 @@ namespace ft
 		//optional
 	//	virtual bool isValid(const IRequest &req, std::string const &http_version) = 0;
 		
-		virtual bool isValid(std::string const & line) = 0;
+		// returns -1 if not valid, 1 if valid and 0 if got only "\r"
+		virtual int isValid(std::string const & line) = 0;
+		virtual int isValid(char * line) = 0;
+
 		virtual void reset(void) = 0;
 
 		virtual ~IRequestValidator(){}
