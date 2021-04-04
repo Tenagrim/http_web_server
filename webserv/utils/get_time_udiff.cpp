820e93bc50c6d32c43993acbbcc4a86af69341fe
Tenagrim <tenagrim@gmail.com>
2021-04-04 19:44:50 +0300

    namespace ft

--- webserv/utils/get_time_udiff.cpp
+++ webserv/utils/get_time_udiff.cpp
@@ -2,12 +2,12 @@
 // Created by Gerry Shona on 4/4/21.
 //
 #include <sys/time.h>
-unsigned long	get_time_udiff(struct timeval *t1, struct timeval *t2)
-{
-	struct timeval diff;
+namespace ft {
+	unsigned long get_time_udiff(struct timeval *t1, struct timeval *t2) {
+		struct timeval diff;
 
-	diff.tv_sec = t2->tv_sec - t1->tv_sec;
-	diff.tv_usec = t2->tv_usec - t1->tv_usec;
-	return (diff.tv_sec * 1000000 + diff.tv_usec);
+		diff.tv_sec = t2->tv_sec - t1->tv_sec;
+		diff.tv_usec = t2->tv_usec - t1->tv_usec;
+		return (diff.tv_sec * 1000000 + diff.tv_usec);
+	}
 }
-
